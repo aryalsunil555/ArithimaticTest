4bd0a9cb1ae63bde8f724529dddc37ff5a6ffe3f
aryalsunil555 <aryalsunil555@gmail.com>
2019-05-29 11:48:45 +0545

    add

--- app/src/main/java/com/arithimatic/MainActivity.java
+++ app/src/main/java/com/arithimatic/MainActivity.java
@@ -2,12 +2,30 @@ package com.arithimatic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+private EditText etFirst, etSecond;
+private Button btnSum,btnSubstract,btnDivide;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        etFirst = findViewById(R.id.etFirst);
+        etSecond = findViewById(R.id.etSecond);
+        btnSum = findViewById(R.id.btnSum);
+        
+        btnSum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Add();
+            }
+
+            private void Add() {
+            }
+        });
+        
     }
 }
